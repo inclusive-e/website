1f37b83cf86ef34e457620b002b228888a8e838e
varunbhaya03 <varunbhaya@gmail.com>
2022-02-17 22:20:27 +0530

    add links and design updates - completed draft 1.0

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -5,7 +5,7 @@ const MainLayout = ({ children, ...props }) => {
   return (
     <Fragment>
       <div className="dark:bg-d-accent font-secondary p-2 md:text-center top-tooltip">
-        Our entire budget and time went into building for our clients. We're
+        Our entire time and budget went into building for our clients. We're
         growing, along with you!{" "}
         <span className="">
           Contact{" "}
